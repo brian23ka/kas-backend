3310c28de3470f1033cc544a8170715f23679e36
Brian Kaloki <brianngenye@gmail.com>
2025-07-16 18:47:07 +0300

    fixing deployment

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ import os
 
 app = create_app()
 
+@app.route("/")
+def index():
+    return "KAS Backend API is running."
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
